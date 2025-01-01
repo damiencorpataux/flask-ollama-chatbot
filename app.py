3f9bd90ec3553736d1c23f52055150369e2e60ac
Damien Corpataux <d@mien.ch>
2025-01-01 21:04:11 +0100

    Factorize chatbot into flask blueprint

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import ollama
 import flask
 from flask_session import Session
+import chatbot
 
 app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = '6e3e77427ceab6bf6649d755'  # FIXME: Move to .env file
@@ -8,52 +8,10 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-model = 'llama3.2'
-
-@app.before_request
-def before_request():
-    try:
-        flask.session['messages']
-    except KeyError:
-        flask.session['messages'] = [
-            {
-                'role': 'system',
-                'content': 'Your mission is to answer questions about Damien Corpataux. Do not answer questions about any other topic. '
-            }
-        ]
-        print('Initialized chat messages:', flask.session['messages'])
-
-@app.route('/')
-def web_ui():
-    return flask.render_template('home.html')
-
-@app.route('/chat/say/<path:question>')
-def api_chat_say(question):
-    flask.session['messages'].append({'role': 'user', 'content': question})
-    response = ollama.chat(
-        model=model,
-        messages=flask.session['messages'],
-        # stream=True,
-    )
-    # def generate():
-    #     for chunk in response:
-    #         print(chunk['message']['content'], end='', flush=True)
-    #         flask.session['messages'][-1]['content'] += chunk['message']['content']
-    #         yield chunk['message']['content']
-    #     print('DONE !!!!!!!!!!!!!!!!!', flask.session['messages'][-1])
-    # return flask.stream_with_context(generate())
-    flask.session['messages'].append({'role': 'assistant', 'content': response['message']['content']})
-    return response['message']['content']
-
-@app.route('/chat/history')
-def api_chat_history():
-    messages = list(filter(
-        lambda m: m.get('role') in ['user', 'assistant'],
-        flask.session['messages']))
-    return flask.jsonify(messages)
-
-@app.route('/chat/clear')
-def api_chat_clear():
-    del flask.session['messages']
-    before_request()
-    return '', 201
+chatbot.initial_messages = initial_messages = [
+    {
+        'role': 'system',
+        'content': 'Your mission is to answer questions about Damien Corpataux. Do not answer questions about any other topic. '
+    }
+]
+app.register_blueprint(chatbot.blueprint)
---
+++ chatbot/__init__.py
@@ -0,0 +1,58 @@
+import ollama
+import flask
+
+blueprint = flask.Blueprint(
+    'chatbot',
+    __name__,
+    template_folder='templates')  # app = flask.Flask(__name__)
+
+model = 'llama3.2'
+initial_messages = [
+    {
+        'role': 'system',
+        'content': 'You are a helpful assistant.'
+    }
+]
+
+@blueprint.before_request
+def before_request():
+    try:
+        flask.session['messages']
+    except KeyError:
+        flask.session['messages'] = initial_messages
+        print('Initialized chat messages:', flask.session['messages'])
+
+@blueprint.route('/')
+def web_ui():
+    return flask.render_template('home.html')
+
+@blueprint.route('/chat/say/<path:question>')
+def api_chat_say(question):
+    flask.session['messages'].append({'role': 'user', 'content': question})
+    response = ollama.chat(
+        model=model,
+        messages=flask.session['messages'],
+        # stream=True,
+    )
+    # def generate():
+    #     for chunk in response:
+    #         print(chunk['message']['content'], end='', flush=True)
+    #         flask.session['messages'][-1]['content'] += chunk['message']['content']
+    #         yield chunk['message']['content']
+    #     print('DONE !!!!!!!!!!!!!!!!!', flask.session['messages'][-1])
+    # return flask.stream_with_context(generate())
+    flask.session['messages'].append({'role': 'assistant', 'content': response['message']['content']})
+    return response['message']['content']
+
+@blueprint.route('/chat/history')
+def api_chat_history():
+    messages = list(filter(
+        lambda m: m.get('role') in ['user', 'assistant'],
+        flask.session['messages']))
+    return flask.jsonify(messages)
+
+@blueprint.route('/chat/clear')
+def api_chat_clear():
+    del flask.session['messages']
+    before_request()
+    return '', 201
