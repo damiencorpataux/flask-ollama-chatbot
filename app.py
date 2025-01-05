4259c66d9934c9c35c7f08050ba37db8f4bdeb24
Damien Corpataux <d@mien.ch>
2025-01-05 19:58:41 +0100

    0.0.3
    App: Add multi-language using google translate
    App: Add response streaming
    Backend: Update prompt
    Backend: Code readability
    UI: Replace github logo <img> with <svg>
    README: Update features and lines of code count

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import chatbot
 import yaml
 import requests
 
-system_message = f'''
+chatbot.languages = ['en', 'fr', 'de']
+
+chatbot.system_message = f'''
 Here is information about Damien Corpataux:
 
 Profile:
@@ -19,11 +21,13 @@ If there the user input is not clear to you, don't comment but simply ask the us
 If asked for, give the link to your CV: [CV en français](https://www.mien.ch/about/cv) and [CV in English](https://www.mien.ch/about/cv/en.html).
 Don't repeat that you are Damien Corpataux.
 Don't repeat greetings.
-Emphasize on your certifications (PMP, Bachelor), technologies, projects and languages (french, english level FCE, german level ZMP).
+Keep your answers concise.
+Emphasize on your certifications (PMP, Bachelor), technologies, projects and languages (french as mother tongue, english with FCE ceertificate, german with ZMP certificate).
 Enphasize on the richness of your work history, adapting and contributing to various roles in multiple business areas and work environments.
 Don't talk about certifications that are not in the CV !
 '''
-first_message = '''
+
+chatbot.first_message = '''
 Hello, I am Damien Corpataux, a skilled professional with a strong background in information systems.
 Please find my CV in [French](https://www.mien.ch/about/cv/fr.html) and [English](https://www.mien.ch/about/cv/en.html).
 
@@ -31,16 +35,6 @@ Let's talk about my work experience, skills and projects.
 Feel free to ask any question !
 '''
 
-chatbot.initial_messages = [
-    {
-        'role': 'system',
-        'content': system_message
-    },
-    {
-        'role': 'assistant',
-        'content': first_message
-    }
-]
 app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = '6e3e77427ceab6bf6649d755'  # FIXME: Move to .env file
 app.config["SESSION_PERMANENT"] = False
