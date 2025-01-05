4e1a8f9f92547bda4c2562676b48a10f581cf437
Damien Corpataux <d@mien.ch>
2025-01-05 19:52:25 +0100

    App: Add multi-language using google translate + Add response streaming

--- chatbot/__init__.py
+++ chatbot/__init__.py
@@ -1,6 +1,9 @@
 import ollama
 import flask
 import flask_limiter
+import requests
+import uuid
+from collections import defaultdict
 
 blueprint = flask.Blueprint(
     'chatbot',
@@ -18,6 +21,7 @@ model = 'llama3.2'
 languages = ['en', 'fr']
 system_message = 'You are a helpful assistant.'
 first_message = 'Hello, how can I help you ?'
+messages_dirty = defaultdict(list)  # A per session.uid dict of messages written to session (not saved because of streaming)
 
 @blueprint.before_request
 def before_request():
@@ -31,11 +35,20 @@ def before_request():
             'content': first_message
         }
     ]
+    # Setup session uid
+    try:
+        flask.session['uid']
+    except KeyError:
+        flask.session['uid'] = uuid.uuid4()
+    # Setup initial messages
     try:
         flask.session['messages']
     except KeyError:
         flask.session['messages'] = initial_messages
         print('Initialized chat messages:', flask.session['messages'])
+    # Add dirty messages (not yet written to session) to actual session
+    flask.session['messages'] += messages_dirty[flask.session['uid']]
+    del messages_dirty[flask.session['uid']]
 
 @blueprint.route('/')
 @blueprint.route('/<lang>')
@@ -53,20 +66,20 @@ def api_chat_say(question):
         options={
             'max_tokens': 32768
         },
-        # stream=True
+        stream=True
     )
-    flask.session['messages'].append({'role': 'assistant', 'content': answer['message']['content']})
-    return answer['message']['content']
+    # flask.session['messages'].append({'role': 'assistant', 'content': answer['message']['content']})
+    # return answer['message']['content']
     # FIXME: Returning streamed answer prevent from writing to session,
     #   due to the http headers already sent.
-    # def generate():
-    #     flask.session['messages'].append({'role': 'assistant', 'content': ''})
-    #     for chunk in answer:
-    #         # print(chunk['message']['content'], end='', flush=True)
-    #         flask.session['messages'][-1]['content'] += chunk['message']['content']
-    #         yield chunk['message']['content']
-    #     print(flask.session['messages'][-1]['content'])
-    # return flask.stream_with_context(generate())
+    def generate():
+        messages_dirty[flask.session['uid']].append({'role': 'assistant', 'content': ''})
+        for chunk in answer:
+            # print(chunk['message']['content'], end='', flush=True)
+            messages_dirty[flask.session['uid']][-1]['content'] += chunk['message']['content']
+            yield chunk['message']['content']
+        print(messages_dirty[flask.session['uid']])
+    return flask.stream_with_context(generate())
 
 @blueprint.route('/chat/history')
 def api_chat_history():
@@ -93,20 +106,37 @@ def api_chat_translate(lang):
         flask.session['last_translated_to'] = lang
 
 def translate(source, destination, text):
-    response = ollama.chat(
-        model=model,
-        messages=[
-            {
-                'role': 'system',
-                'content': f'You are a language translator. Please translate the user input from {source} to {destination} without any comment or remark. Just output the translated text. Use a very polite and inviting tone.'
-            },
-            {
-                'role': 'user',
-                'content': text
-            }
-        ],
-        options={
-            # 'max_tokens': 32768
-        }
-    )
-    return response['message']['content']
+    import random
+    response = requests.get(
+        'https://translate.google.so/translate_a/t',
+        params={
+            'q': text,
+            'tl': destination,
+            'sl': 'auto',
+            'client': hex(random.getrandbits(128))[2:],
+            'ie': 'UTF-8',
+            'oe': 'UTF-8',
+            'tbb': 1
+    })
+    if response.ok:
+        return response.json()[0][0]
+    else:
+        return f'Could not translate text ({response.status_code})\n\n{text}'
+        # raise RuntimeError('Could not translate:', response.text)
+    # response = ollama.chat(
+    #     model=model,
+    #     messages=[
+    #         {
+    #             'role': 'system',
+    #             'content': f'You are a language translator. Please translate the user input from {source} to {destination} without any comment or remark. Just output the translated text. Use a very polite and inviting tone.'
+    #         },
+    #         {
+    #             'role': 'user',
+    #             'content': text
+    #         }
+    #     ],
+    #     options={
+    #         # 'max_tokens': 32768
+    #     }
+    # )
+    # return response['message']['content']
